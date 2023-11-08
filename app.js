21b2fbd35efbf3a3734b5ea50831dac7230232a9
emiferreyra89 <ferreyra89@gmail.com>
2023-11-08 15:59:46 -0300

    modifique el codigo para redireccionar las cargas de las paginas registro y login a la pagina home

--- app.js
+++ app.js
@@ -1,9 +1,32 @@
+const { log } = require("console");
 const express = require("express");
+const bodyParser = require('body-parser');
 const app = express();
 const path = require("path");
 
 app.get("/",(req,res) => {
     res.sendFile(path.join(__dirname,"/views/home.html"))
+})
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.get("/registro",(req,res) => {
+    res.sendFile(path.join(__dirname,"/views/register.html"))
+})
+
+app.post("/registro",(req,res) => {
+    console.log(req.body);
+    res.redirect("/");
+})
+
+app.get("/login",(req,res) => {
+    res.sendFile(path.join(__dirname,"/views/login.html"))
+})
+
+app.post("/login",(req,res) => {
+    console.log(req.body);
+    res.redirect("/");
 })
 
 app.use(express.static(__dirname+"/public"));
